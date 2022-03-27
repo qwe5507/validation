85158c6b924ee5e2446e4aca36740229f12e564c
qwe5507 <apvmffkdls@naver.com>
2022-03-27 15:19:33 +0900

    BindingResult1

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,6 +3,8 @@ package hello.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class ItemServiceApplication {
 
