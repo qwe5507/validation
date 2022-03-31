4049ac26c0a080167d1a0312ec7a0508cbed9f0d
qwe5507 <apvmffkdls@naver.com>
2022-03-31 21:19:19 +0900

    Bean Validation - 한계

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+    @NotNull //수정 요구사항 추가
     private Long id;
 
     @NotBlank
@@ -20,7 +21,7 @@ public class Item {
     @Range(min = 1000, max = 1000000)
     private Integer price;
     @NotNull
-    @Max(9999)
+//    @Max(9999) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
